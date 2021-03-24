e194a57c99c868dcd039ee1b99aadb58e785f004
gilbertogj23@gmail.com <gilbertogj@DESKTOP-BBSNBM5.localdomain>
2021-03-24 09:42:55 -0600

    reverse urls, pets

--- dogtor/vet/views.py
+++ dogtor/vet/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import View, TemplateView, ListView, DetailView 
 
-from .models import PetOwner
+from .models import PetOwner, Pet
 
 # Create your views here.
 # def list_pet_owners(request):
@@ -41,3 +41,13 @@ class OwnersDetail(DetailView):
     model = PetOwner
     template_name = "vet/owners/detail.html"
     context_object_name = "owner"
+
+class PetsList(ListView):
+    model = Pet
+    template_name = "vet/pets/list.html"
+    context_object_name = "pets"
+
+class PetsDetail(DetailView):
+    model = Pet
+    template_name = "vet/pets/detail.html"
+    context_object_name = "pet"
